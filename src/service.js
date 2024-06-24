3a7c170a19e23d48c4af90f897cb3cb875ea7e25
AvigailH <154588429+AvigailH@users.noreply.github.com>
2024-06-24 21:59:49 +0300

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
-// const apiUrl = process.env.REACT_APP_API;
-const apiUrl =`https://app-5cedf3e4-31df-4667-8c0a-403aa64092f3.cleverapps.io`;
+const apiUrl = `https://app-5cedf3e4-31df-4667-8c0a-403aa64092f3.cleverapps.io`;
 console.log(apiUrl);
 const defaultAxios = axios.create({
     baseURL: apiUrl,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*', // כותרת זו כדי להתייחס ל-CORS
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // וכותרת זו עם הפעולות המותרות
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept', // וכותרת זו לכותרות מסוימות
+        'Access-Control-Allow-Origin': '*', // This header to refer to CORS
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // and this header with the allowed operations
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept', // and this header for certain headers
     },
 });
 

620ffab80cd3346d194e7a234f8a8c324a171244
AvigailH <154588429+AvigailH@users.noreply.github.com>
2024-06-24 22:26:30 +0300

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 // הגדרת כתובת ה-URL הבסיסית של ה-API
-const apiUrl = `https://app-5cedf3e4-31df-4667-8c0a-403aa64092f3.cleverapps.io`;
+const apiUrl = 'https://app-5cedf3e4-31df-4667-8c0a-403aa64092f3.cleverapps.io';
 
-// יצירת instance של axios עם כתובת הבסיס ועם כותרות נכונות
 const defaultAxios = axios.create({
     baseURL: apiUrl,
     headers: {
@@ -12,7 +11,6 @@ const defaultAxios = axios.create({
     }
 });
 
-// הוספת interceptors ל-axios ללכידת תגובות ושגיאות
 defaultAxios.interceptors.response.use(
     response => response,
     error => {
@@ -21,33 +19,31 @@ defaultAxios.interceptors.response.use(
     }
 );
 
-// פונקציה להדפסת בקשות ב-console לצורכי בדיקה
 const logRequest = (method, url, data) => {
     console.log(`Request Method: ${method}`);
     console.log(`Request URL: ${defaultAxios.defaults.baseURL}${url}`);
     if (data) console.log(`Request Data: ${JSON.stringify(data)}`);
 };
 
-// פונקציות שירות המבצעות את הבקשות לשרת
 export default {
     getTasks: async () => {
         logRequest('GET', '/items');
-        const result = await defaultAxios.get('/items');
+        const result = await defaultAxios.get(`${apiUrl}/items`);
         return result.data;
     },
     addTask: async (name) => {
         logRequest('POST', '/', { name });
-        const result = await defaultAxios.post('/', { name });
+        const result = await defaultAxios.post(`${apiUrl}/`, { name });
         return result.data;
     },
     setCompleted: async (id, isComplete) => {
         logRequest('PUT', `/${id}`, { isComplete });
-        const result = await defaultAxios.put(`/${id}`, { isComplete });
+        const result = await defaultAxios.put(`${apiUrl}/${id}`, { isComplete });
         return result.data;
     },
     deleteTask: async (id) => {
         logRequest('DELETE', `/${id}`);
-        await defaultAxios.delete(`/${id}`);
+        await defaultAxios.delete(`${apiUrl}/${id}`);
         return { success: true };
     }
 };

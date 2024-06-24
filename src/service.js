2d38726a148acf29096daaad6fba315917284468
AvigailH <154588429+AvigailH@users.noreply.github.com>
2024-06-24 21:20:59 +0300

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiUrl = process.env.REACT_APP_API;
-console.log(apiUrl);
+console.log("שלום");
 const defaultAxios = axios.create({
     baseURL: apiUrl,
     headers: {

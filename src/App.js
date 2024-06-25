00096b0686ffa7588260bb3c5f838303066d7cbb
AvigailH <154588429+AvigailH@users.noreply.github.com>
2024-06-25 11:25:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { observer } from 'mobx-react-lite';
 import service from './service.js';
 
-const App = observer(() => {
-    const [newTodo, setNewTodo] = useState("");
+function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-    useEffect(() => {
-        service.initTodos();
-    }, []);
+  async function getTodos() {
+    const todos = await service.getTasks();
+    setTodos(todos);
+  }
 
-    async function createTodo(e) {
-        e.preventDefault();
-        await service.addTodo(newTodo);
-        setNewTodo("");
-    }
+  async function createTodo(e) {
+    e.preventDefault();
+    await service.addTask(newTodo);
+    setNewTodo("");//clear input
+    await getTodos();//refresh tasks list (in order to see the new one)
+  }
 
-    async function updateCompleted(todo, isComplete) {
-        await service.updateCompleted(todo.id, isComplete);
-    }
+  async function updateCompleted(todo, isComplete) {
+    await service.setCompleted(todo.id, isComplete);
+    await getTodos();//refresh tasks list (in order to see the updated one)
+  }
 
-    async function deleteTodo(id) {
-        await service.deleteTodo(id);
-    }
+  async function deleteTodo(id) {
+    await service.deleteTask(id);
+    await getTodos();//refresh tasks list
+  }
 
-    return (
-        <section className="todoapp">
-            <header className="header">
-                <h1>todos</h1>
-                <form onSubmit={createTodo}>
-                    <input className="new-todo" placeholder="Well, let's take on the day" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-                </form>
-            </header>
-            <section className="main" style={{ display: "block" }}>
-                <ul className="todo-list">
-                    {service.getTodos.map(todo => {
-                        return (
-                            <li className={todo.isComplete ? "completed" : ""} key={todo.id}>
-                                <div className="view">
-                                    <input className="toggle" type="checkbox" checked={todo.isComplete} onChange={(e) => updateCompleted(todo, e.target.checked)} />
-                                    <label>{todo.name}</label>
-                                    <button className="destroy" onClick={() => deleteTodo(todo.id)}></button>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </section>
-        </section>
-    );
-});
+  useEffect(() => {
+    getTodos();
+  }, []);
 
-export default App;
+  return (
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <form onSubmit={createTodo}>
+          <input className="new-todo" placeholder="Well, let's take on the day" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+        </form>
+      </header>
+      <section className="main" style={{ display: "block" }}>
+        <ul className="todo-list">
+          {todos.map(todo => {
+            return (
+              <li className={todo.isComplete ? "completed" : ""} key={todo.id}>
+                <div className="view">
+                  <input className="toggle" type="checkbox" defaultChecked={todo.isComplete} onChange={(e) => updateCompleted(todo, e.target.checked)} />
+                  <label>{todo.name}</label>
+                  <button className="destroy" onClick={() => deleteTodo(todo.id)}></button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </section >
+  );
+}
